791ba9ce31ca9d1035b0573b01309c4115edf0e6
amir belhadj <belhadj.amir@esprit.tn>
2025-04-10 03:20:21 +0200

    amir integration

---
+++ app.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+
+const superAdminRoute = require('./routes/superAdminRoutes');
+const userRoutes = require('./routes/user');
+const emailRoutes = require('./routes/email'); 
+const usersRoutes = require('./routes/adminRoute');
+const paymentRoutes = require('./routes/paymentRoutes');
+const dishRoutes = require('./routes/dish');
+// Middleware
+app.use(cors({
+    origin: '*',  // URL de ton frontend
+    //credentials: true, // Allow cookies and authentication headers
+    //methods: "GET, POST, PUT, DELETE, OPTIONS",
+    allowedHeaders: 'Content-Type,Authorization',
+})); // Enable CORS for frontend requests
+app.use(express.json());
+
+
+app.use('/api/user', userRoutes); 
+app.use('/api/email', emailRoutes); 
+app.use("/superAdmin",superAdminRoute)
+app.use('/api/users', usersRoutes);  // /api/users pour gérer les utilisateurs
+app.use('/api/payment', paymentRoutes);  // /api/users pour gérer les utilisateurs
+app.use('/api/dish', dishRoutes); 
+
+
+// Lancement du serveur
+app.listen(port, () => {
+    console.log(` Serveur démarré sur http://localhost:${port}`);
+});
+
+
+// Connect to the MongoDB database
+mongoose.connect('mongodb+srv://kenza:kenza2020@cluster0.65hm7.mongodb.net/themenufy?retryWrites=true&w=majority&appName=Cluster0', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('\x1b[33m%s\x1b[0m', `Connected to MongoDB`)
+    ;})
+  .catch((err) => {
+    console.log('\x1b[31m%s\x1b[0m','error mongodb connection',err)
+  });
+
